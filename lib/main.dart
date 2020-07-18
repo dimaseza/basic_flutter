4239b085cff7dbbfdf28b597ecf6f073d498fed8
dimaseza <dimasandre18@gmail.com>
2020-07-18 18:35:53 +0700

    commit 2 basic animation

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:docs_flutter/animation/animation_page_route.dart';
+import 'package:docs_flutter/animation/animation_physics.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      // You can change this home what you want.
+      // Just call root name widget state on every file.
+      home: PhysicCardDragDemo(),
+    ),
+  );
+}
